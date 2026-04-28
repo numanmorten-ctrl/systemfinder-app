dbfc27e6a58fb80cc7356088afc1335d4fc551ce
numanmorten-ctrl <numan.morten@gmail.com>
2026-04-28 21:29:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ st.title("System sammenligning")
 
 df = pd.read_excel("10_list.xlsx", header=1)
 
-# ryd data
+# gør kolonnenavne unikke
+df.columns = pd.io.parsers.ParserBase({'names': df.columns})._maybe_dedup_names(df.columns)
+
+# ryd systemnavne
 systemer = df["System_Variant_Name_Local_sys_desc_pdm_gpdm"].dropna()
 systemer = systemer[systemer != "Optional"]
 systemer = systemer.unique()
@@ -17,5 +20,7 @@ valg = st.multiselect(
 
 if valg:
     comp = df[df["System_Variant_Name_Local_sys_desc_pdm_gpdm"].isin(valg)]
+
     comp = comp.set_index("System_Variant_Name_Local_sys_desc_pdm_gpdm").T
+
     st.dataframe(comp)
