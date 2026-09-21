7d7f80ef1718d791d676c5ba410b0d55a638fd4e
numanmorten-ctrl <numan.morten@gmail.com>
2026-09-21 19:00:52 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -649,11 +649,11 @@ def show_tab(rows):
                ),
            )
        else:
-st.info(
+           st.info(
                "Ingen data"
            )
    else:
-st.info(
+       st.info(
            "Ingen data"
        )
 
