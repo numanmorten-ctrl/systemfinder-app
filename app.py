e76afa8d691b383634b7ef0475d8d6c4e7ce9adb
numanmorten-ctrl <numan.morten@gmail.com>
2026-09-21 15:29:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -526,9 +526,9 @@ def show_tab(rows):
                height=100 + len(df_show) * 35,
            )
        else:
-st.info("Ingen data")
+           st.info("Ingen data")
    else:
-st.info("Ingen data")
+       st.info("Ingen data")
 
 tab1, tab2, tab3, tab4 = st.tabs(
    [
